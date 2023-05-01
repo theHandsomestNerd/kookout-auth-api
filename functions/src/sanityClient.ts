49aff7278d9206b18a9cd18b95e6ff3a4ff494ee
Mr. Singleton <terrell.singleton@gmail.com>
2023-05-01 06:31:37 -0400

    WIP

--- functions/src/sanityClient.ts
+++ functions/src/sanityClient.ts
@@ -2,11 +2,12 @@
 const client = require("@sanity/client")
 const functions = require('firebase-functions')
 
+console.log(functions.config());
 const sanityClient = client({
-    projectId: functions.config()?.sanity?.project_id ?? process.env.SANITY_STUDIO_API_PROJECT_ID,
-    dataset: functions.config()?.sanity?.dataset ?? process.env.SANITY_STUDIO_API_APIDATASET,
-    apiVersion: functions.config()?.sanity?.version ?? process.env.SANITY_STUDIO_API_VERSION,
-    token: functions.config()?.sanity?.token ?? process.env.SANITY_API_TOKEN,
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID ?? functions.config().sanity.project_id,
+    dataset: process.env.SANITY_STUDIO_API_APIDATASET ?? functions.config().sanity.dataset,
+    apiVersion: process.env.SANITY_STUDIO_API_VERSION ?? functions.config().sanity.theversion,
+    token: process.env.SANITY_API_TOKEN ?? functions.config().sanity.token,
     useCdn: false
 })
 
