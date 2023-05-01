8410e0f84cea32555304010138c6a170f5b4dc87
Mr. Singleton <terrell.singleton@gmail.com>
2023-05-01 07:02:28 -0400

    Fix some github actions erros with env variables

--- functions/src/sanityClient.ts
+++ functions/src/sanityClient.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const client = require("@sanity/client")
+const functions = require('firebase-functions')
 
 const sanityClient = client({
-    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_API_APIDATASET,
-    apiVersion: process.env.SANITY_STUDIO_API_VERSION,
-    token: process.env.SANITY_API_TOKEN,
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID ?? functions.config().SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_APIDATASET ?? functions.config().SANITY_STUDIO_API_APIDATASET,
+    apiVersion: process.env.SANITY_STUDIO_API_VERSION ?? functions.config().SANITY_STUDIO_API_VERSION,
+    token: process.env.SANITY_API_TOKEN ?? functions.config().SANITY_API_TOKEN,
     useCdn: false
 })
 
