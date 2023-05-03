94f0b516f025dbe1096b6c7fa48f9912c4a8fed0
Mr. Singleton <terrell.singleton@gmail.com>
2023-05-03 15:52:20 -0400

    added functions config
    must addd functions config:set sanity.XXXX to add these env variables

--- functions/src/sanityClient.ts
+++ functions/src/sanityClient.ts
@@ -3,10 +3,10 @@ const client = require("@sanity/client")
 const functions = require('firebase-functions')
 
 const sanityClient = client({
-    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID ?? functions.config().SANITY_STUDIO_API_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_API_APIDATASET ?? functions.config().SANITY_STUDIO_API_APIDATASET,
-    apiVersion: process.env.SANITY_STUDIO_API_VERSION ?? functions.config().SANITY_STUDIO_API_VERSION,
-    token: process.env.SANITY_API_TOKEN ?? functions.config().SANITY_API_TOKEN,
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID ?? functions.config()?.sanity?.project_id ?? "xxxx",
+    dataset: process.env.SANITY_STUDIO_API_APIDATASET ?? functions.config()?.sanity?.dataset,
+    apiVersion: process.env.SANITY_STUDIO_API_VERSION ?? functions.config()?.sanity?.theversion ?? 1,
+    token: process.env.SANITY_API_TOKEN ?? functions.config()?.sanity?.token
     useCdn: false
 })
 
